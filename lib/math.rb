897fe3d690cff64dd20c2076455b6f347494fc8c
sofia tofigh <sst2140@columbia.edu>
2020-03-29 00:22:21 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+ sum=num1+num2
+ sum
 end
 
 def subtraction(num1, num2)
+  difference=num1-num2
+  difference
 
 end
 
 def division(num1, num2)
+  dividend=num1/num2
+  dividend
 
 end
 
 def multiplication(num1, num2)
+  product=num1*num2
+  product
 
 end
 
 def modulo(num1, num2)
+  quotient=num1%num2
+  quotient
 
 end
 
 def square_root(num)
+  root= Math.sqrt(num)
+  root
 
 end
